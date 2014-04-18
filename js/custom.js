d804429a0e6c2a5ca6ff6eb1fb26ba6dc086a1d3
Pritam Pebam <ptamzz@gmail.com>
2014-04-18 20:49:48 +0530

    Code refactored
    Improved page Loading sequence

--- js/custom.js
+++ js/custom.js
@@ -11,25 +11,22 @@ var page, //preset url hash value for home
 	view = false,	//false if loading for the first time
 	viewPane,
 	hover = false,	//pointer ouside .main-nav
-	mouseX, mouseY = 0;
+	dom,
+	mouseX, mouseY = 0,
+	winHeight, winWidth;
 
 $(function() {
 
-	var winHeight = $(window).height(),
-		winWidth = $(window).width();
+	dom = $('body, html');
+	winHeight = $(window).height();
+	winWidth = $(window).width();
+	
 
 	//Initial URL handling
 	page = getPage(window.location.hash); //return page from url
+	viewPane = (page != '' ? $("." + page+"-pane") : $('.home-pane') );	
 
-	//page =  window.location.hash.substr(2);
-	if(page != '') {
-		viewPane = $("." + page+"-pane");	//Set current viewPane when loaded with hash URL
-	} else {
-		viewPane = $('.home-pane');	//initial pane in view
-	}
-
-	loadPage(page); //Load New Page
-	doMagicTransition(viewPane);	//Show ViewPane
+	doMagicTransition(viewPane, page);	//Show ViewPane
 
 	//HashBang handling without the "Bang" of course
 	$(window).bind('hashchange', function() {
@@ -37,15 +34,13 @@ $(function() {
 		page =  getPage(window.location.hash);	//Get URL hash value "#/hashvalue" & remove "#/" 	
 
 		var e = $("."+page+"-pane");	//Get New viewPane
-		doMagicTransition(e);
 
-		loadPage(page); //Load New Page
+		doMagicTransition(e, page);
 	});
 
-	//Setting viewport heights
+	//Setting viewport/container attributes
 	$('.st-menu, .main-nav').css({ height: winHeight });
 	$('.content').css({  'width' : winWidth, 'height': winHeight });
-
 	$('.logo-container, .body-bar').css({ 'top' : (winHeight/2) - 200 })
 
 	//Transition animation
@@ -53,7 +48,7 @@ $(function() {
 
 	$('.main-nav').click(function(e){ mouseY = e.clientY; });	//Set mouse-click coords - used to determine pointer move distance
 
-	//If pointer moves more than 20px, call hoverIn
+	//If pointer moves more than 20px on y-axis, call hoverIn
 	$('.main-nav').mousemove(function(e){
       if (Math.abs(mouseY - e.clientY) > 20 ){
       	hoverIn();
@@ -143,18 +138,19 @@ function hoverOut(){
 
 
 //Main Page Transition to new viewPane
-function doMagicTransition(e){
-	console.log("doMagicTransition");
+function doMagicTransition(e, page){
 	//Show the pane to be flown in
 	e.show();
-	var dom = $('body, html');
+	$(".content").html('');	//Remove content
+
 	dom.css({ 'overflow' : 'auto' });
 	dom.scrollTop(viewPane.offset().top);	//Set scroll position to the current viewPane
 
 	dom.animate({
         scrollTop: e.offset().top
     }, 500, function(){
-    	
+    	loadPage(page);	//Load Page content
+
     	hoverOut();
 
     	if(view){
@@ -167,8 +163,6 @@ function doMagicTransition(e){
 }
 
 function loadPage(p){
-	console.log("LoadPage Called");
-
 	$.ajax({
 		type: "POST",
 		url: "php/loadPage.php",
@@ -189,14 +183,17 @@ function loadPage(p){
 
 
 function getPage(hash){
-	var url;
-	if (hash.charAt(1) == "/" ){
-		var url = hash.substr(2);		//To handle new URL  (format '#/page-name')
-	} else {
-		var url = hash.substr(1);		//To handle old url, format '#/page-name'
-	}
-
+	var url = (hash.charAt(1) == "/" ? hash.substr(2) : hash.substr(1)); //To work for both #/page-name & #page-name URL structure
 	return url;
-}	
+}
+
+
+/* 
+function mobilecheck() {
+	var check = false;
+	(function(a){if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+	return check;
+}
+*/
 
 
